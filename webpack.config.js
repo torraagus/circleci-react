14be51af45ac9952a634628f35719d31e93ce20a
Torra Agustin <torra.agustinignacio@gmail.com>
2020-03-18 18:02:21 -0300

    comment some code that didn't work

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,23 @@ module.exports = {
             }
           }
         },
+        /*{
+          test: /\.(scss|sass|css)$/,
+          exclude: /node_modules/,
+          loaders: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 1,
+                localIdentName: '[local]___[hash:base64:5]'
+              }
+            },
+          'sass-loader',
+          ]
+        }*/
         { 
           test: /\.css$/, 
           loader: [
