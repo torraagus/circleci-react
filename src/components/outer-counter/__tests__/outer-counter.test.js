c57d916acd4b4d8e0fcddf161312b1d3890f4f41
Torra Agustin <torra.agustinignacio@gmail.com>
2020-03-20 18:32:45 -0300

    add snapshot testing

--- src/components/outer-counter/__tests__/outer-counter.test.js
+++ src/components/outer-counter/__tests__/outer-counter.test.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import OuterCounter from './../../outer-counter/outer-counter.component';
 
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+
+import renderer from 'react-test-renderer';
+
+afterEach(cleanup);
 
 it('should render component without crashing', () => {
     let div = document.createElement('div');
     ReactDOM.render(<OuterCounter></OuterCounter>, div);
+})
+
+it('matches snapshot', () => {
+    const tree = renderer.create(<OuterCounter></OuterCounter>).toJSON();
+    expect(tree).toMatchSnapshot();
 })
