ccabdcf1424bbc7f0f78aa2f0d1b45701629ea85
Torra Agustin <torra.agustinignacio@gmail.com>
2020-03-21 17:00:22 -0300

    remove link component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-//import Link from './components/link/link.component';
 import OuterCounter from './components/outer-counter/outer-counter.component';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Root extends Component {
   render() {
     return (
-      //<Link page="http://www.facebook.com">Go to Facebook</Link>
       <OuterCounter></OuterCounter>
     )
   }
